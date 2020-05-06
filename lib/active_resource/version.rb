5515e75f4bcfa5cd122d778fc458e22ca52757d2
Tobias Kraze <tobias.kraze@makandra.de>
2020-05-06 11:52:26 +0200

    bump version

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -2,7 +2,7 @@ module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 4
     MINOR = 1
-    TINY  = 0
+    TINY  = 1
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
